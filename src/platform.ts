a5fb389f720ea27053ac2990180dfcf380e36e4a
Chris J. Shull <chrisjshull@gmail.com>
2021-09-28 23:28:27 -0700

    initial working plugin

--- src/platform.ts
+++ src/platform.ts
@@ -1,7 +1,9 @@
 import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
 
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
-import { JuiceBoxPlatformAccessory } from './platformAccessory';
+import { JuiceBoxPlatformAccessoryHandler } from './platformAccessory';
+
+import juicenet from 'node-juicenet';
 
 /**
  * HomebridgePlatform
@@ -15,6 +17,13 @@ export class JuiceBoxHomebridgePlatform implements DynamicPlatformPlugin {
   // this is used to track restored cached accessories
   public readonly accessories: PlatformAccessory[] = [];
 
+  public Volts: any; // todo type as Characteristic
+  public Amperes: any; // todo type as Characteristic
+  public Watts: any; // todo type as Characteristic
+  public KilowattHours: any; // todo type as Characteristic
+  public VoltAmperes: any; // todo type as Characteristic
+  public KilowattVoltAmpereHour: any; // todo type as Characteristic
+
   constructor(
     public readonly log: Logger,
     public readonly config: PlatformConfig,
@@ -22,14 +31,43 @@ export class JuiceBoxHomebridgePlatform implements DynamicPlatformPlugin {
   ) {
     this.log.debug('Finished initializing platform:', this.config.name);
 
+    const makeCharacteristic = (name: string, unit: string, minStep: number, uuid: string, format = api.hap.Formats.FLOAT) => {
+      return class extends this.Characteristic {
+        public static readonly UUID: string = uuid;
+        constructor() {
+          super(name, uuid, {
+            format,
+            minValue: 0,
+            maxValue: 65535,
+            unit,
+            minStep,
+            perms: [api.hap.Perms.PAIRED_READ, api.hap.Perms.NOTIFY],
+          });
+          this.value = this.getDefaultValue();
+        }
+      };
+    };
+    // https://github.com/snowman715/homebridge_tplink_kasa/tree/50a3b8c84feb400c9f687439fa77f7ef8b039a8b/src/characteristics
+    this.Volts = makeCharacteristic('Volts', 'V', 0.1, 'E863F10A-079E-48FF-8F27-9C2605A29F52');
+    this.Amperes = makeCharacteristic('Amperes', 'A', 0.01, 'E863F126-079E-48FF-8F27-9C2605A29F52');
+    this.Watts = makeCharacteristic('Consumption', 'W', 0.1, 'E863F10D-079E-48FF-8F27-9C2605A29F52');
+    this.KilowattHours = makeCharacteristic('Total Consumption', 'kWh', 0.001, 'E863F10C-079E-48FF-8F27-9C2605A29F52');
+    this.VoltAmperes = makeCharacteristic('Apparent Power', 'VA', 0.1, 'E863F10C-079E-48FF-8F27-9C2605A29F52');
+    this.KilowattVoltAmpereHour = makeCharacteristic('Apparent Energy', 'kVAh', 1, 'E863F10C-079E-48FF-8F27-9C2605A29F52', api.hap.Formats.UINT32);
+
+
     // When this event is fired it means Homebridge has restored all cached accessories from disk.
     // Dynamic Platform plugins should only register new accessories after this event was fired,
     // in order to ensure they weren't added to homebridge already. This event can also be used
     // to start discovery of new accessories.
-    this.api.on('didFinishLaunching', () => {
+    this.api.on('didFinishLaunching', async () => {
       log.debug('Executed didFinishLaunching callback');
       // run the method to discover / register your devices as accessories
-      this.discoverDevices();
+      try {
+        await this.discoverDevices();
+      } catch (e) {
+        this.log.error(e);
+      }
     });
   }
 
@@ -45,60 +83,49 @@ export class JuiceBoxHomebridgePlatform implements DynamicPlatformPlugin {
   }
 
   /**
-   * This is an example method showing how to register discovered accessories.
+   * Register discovered accessories.
    * Accessories must only be registered once, previously created accessories
    * must not be registered again to prevent "duplicate UUID" errors.
    */
-  discoverDevices() {
-
-    // EXAMPLE ONLY
-    // A real plugin you would discover accessories from the local network, cloud services
-    // or a user-defined array in the platform config.
-    const exampleDevices = [
-      {
-        exampleUniqueId: 'ABCD',
-        exampleDisplayName: 'Bedroom',
-      },
-      {
-        exampleUniqueId: 'EFGH',
-        exampleDisplayName: 'Kitchen',
-      },
-    ];
+  async discoverDevices() {
+    const cachedNotFound = new Set(this.accessories);
+    const {devices, response} = await juicenet.getDevicesAsync(this.config.apiToken);
+    if (!response.body.success) {
+      this.log.error(response.body);
+      throw new Error('Could not connect to JuiceNet.');
+    }
 
     // loop over the discovered devices and register each one if it has not already been registered
-    for (const device of exampleDevices) {
-
+    for (const device of devices) {
       // generate a unique id for the accessory this should be generated from
       // something globally unique, but constant, for example, the device serial
       // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.exampleUniqueId);
+      const uuid = this.api.hap.uuid.generate(device.unit_id);
 
       // see if an accessory with the same uuid has already been registered and restored from
       // the cached devices we stored in the `configureAccessory` method above
       const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
+      if (existingAccessory) {
+        cachedNotFound.delete(existingAccessory);
+      }
 
       if (existingAccessory) {
         // the accessory already exists
         this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
 
         // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
-        // existingAccessory.context.device = device;
-        // this.api.updatePlatformAccessories([existingAccessory]);
+        existingAccessory.context.device = device;
+        this.api.updatePlatformAccessories([existingAccessory]);
 
         // create the accessory handler for the restored accessory
         // this is imported from `platformAccessory.ts`
-        new JuiceBoxPlatformAccessory(this, existingAccessory);
-
-        // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
-        // remove platform accessories when no longer present
-        // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
-        // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
+        new JuiceBoxPlatformAccessoryHandler(this, existingAccessory);
       } else {
         // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new accessory:', device.exampleDisplayName);
+        this.log.info('Adding new accessory:', device.name);
 
         // create a new accessory
-        const accessory = new this.api.platformAccessory(device.exampleDisplayName, uuid);
+        const accessory = new this.api.platformAccessory(device.name, uuid);
 
         // store a copy of the device object in the `accessory.context`
         // the `context` property can be used to store any data about the accessory you may need
@@ -106,11 +133,18 @@ export class JuiceBoxHomebridgePlatform implements DynamicPlatformPlugin {
 
         // create the accessory handler for the newly create accessory
         // this is imported from `platformAccessory.ts`
-        new JuiceBoxPlatformAccessory(this, accessory);
+        new JuiceBoxPlatformAccessoryHandler(this, accessory);
 
         // link the accessory to your platform
         this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
       }
+    }
+
+    for (const existingAccessory of cachedNotFound) {
+      // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
+      // remove platform accessories when no longer present
+      this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
+      this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
     }
   }
 }
--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -2,89 +2,137 @@ import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
 
 import { JuiceBoxHomebridgePlatform } from './platform';
 
+import juicenet from 'node-juicenet';
+
 /**
  * Platform Accessory
  * An instance of this class is created for each accessory your platform registers
  * Each accessory may expose multiple services of different service types.
  */
-export class JuiceBoxPlatformAccessory {
+export class JuiceBoxPlatformAccessoryHandler {
+  private pollTimeoutId: NodeJS.Timeout|null = null;
+
   private service: Service;
 
-  private states = {
-    // On: false,
-    // Brightness: 100,
+  private state: Record<string, any> = {
   };
+
+  private overrides: Record<string, any> = {
+  };
+
+  private error: unknown = null;
+
 
   constructor(
     private readonly platform: JuiceBoxHomebridgePlatform,
     private readonly accessory: PlatformAccessory,
   ) {
+    const Characteristic = platform.Characteristic;
 
     // set accessory information
     this.accessory.getService(this.platform.Service.AccessoryInformation)!
-      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Default-Manufacturer')
-      .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
-      .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
+      .setCharacteristic(Characteristic.Manufacturer, 'Enel X')
+      .setCharacteristic(Characteristic.Model, 'JuiceBox [Unknown]')
+      .setCharacteristic(Characteristic.SerialNumber, accessory.context.device.unit_id);
 
-    // get the LightBulb service if it exists, otherwise create a new LightBulb service
+    // get the Outlet service if it exists, otherwise create a new Outlet service
     // you can create multiple services for each accessory
-    this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
+    this.service = this.accessory.getService(this.platform.Service.Outlet) || this.accessory.addService(this.platform.Service.Outlet);
 
     // set the service name, this is what is displayed as the default name on the Home app
     // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
+    this.service.setCharacteristic(Characteristic.Name, accessory.context.device.name);
 
     // each service must implement at-minimum the "required characteristics" for the given service type
     // see https://developers.homebridge.io/#/service/Lightbulb
 
     // register handlers for the On/Off Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setOn.bind(this))                // SET - bind to the `setOn` method below
-      .onGet(this.getOn.bind(this));               // GET - bind to the `getOn` method below
+    this.service.getCharacteristic(Characteristic.On)
+      .onSet(this.setOn.bind(this))
+      .onGet(this.getOn.bind(this));
 
-    // register handlers for the Brightness Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-      .onSet(this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
+    this.service.getCharacteristic(Characteristic.OutletInUse)
+      .onGet(this.getOutletInUse.bind(this));
 
-    /**
-     * Creating multiple services of the same type.
-     *
-     * To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
-     * when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
-     * this.accessory.getService('NAME') || this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE_ID');
-     *
-     * The USER_DEFINED_SUBTYPE must be unique to the platform accessory (if you platform exposes multiple accessories, each accessory
-     * can use the same sub type id.)
-     */
+    this.service.getCharacteristic(this.platform.Volts)
+      .onGet(this.getVolts.bind(this));
+    this.service.getCharacteristic(this.platform.Amperes)
+      .onGet(this.getAmperes.bind(this));
+    this.service.getCharacteristic(this.platform.Watts)
+      .onGet(this.getWatts.bind(this));
+    this.service.getCharacteristic(this.platform.KilowattHours)
+      .onGet(this.getKilowattHours.bind(this));
+    // this.service.getCharacteristic(this.platform.VoltAmperes)
+    //   .onGet(this.getVoltAmperes.bind(this));
+    // this.service.getCharacteristic(this.platform.KilowattVoltAmpereHour)
+    //   .onGet(this.getKilowattVoltAmpereHour.bind(this));
 
-    // Example: add two "motion sensor" services to the accessory
-    const motionSensorOneService = this.accessory.getService('Motion Sensor One Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
 
-    const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
+    this.pausePolling(0);
+  }
 
-    /**
-     * Updating characteristics values asynchronously.
-     *
-     * Example showing how to update the state of a Characteristic asynchronously instead
-     * of using the `on('get')` handlers.
-     * Here we change update the motion sensor trigger states on and off every 10 seconds
-     * the `updateCharacteristic` method.
-     *
-     */
-    let motionDetected = false;
-    setInterval(() => {
-      // EXAMPLE - inverse the trigger
-      motionDetected = !motionDetected;
+  private pausePolling(ms = 1000) {
+    this.pollTimeoutId && clearTimeout(this.pollTimeoutId);
+    this.pollTimeoutId = setTimeout(() => {
+      this.pausePolling(10000);
+      this.poll();
+    }, ms);
+  }
 
-      // push the new value to HomeKit
-      motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
-      motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
+  private async poll() {
+    this.platform.log.debug('Polling...');
+    try {
+      const startPollTimeoutId = this.pollTimeoutId;
+      const {chargerState} = await this.handleJuiceNetErrors(juicenet.getDeviceStateAsync(this.platform.config.apiToken, this.accessory.context.device.token));
+      if (startPollTimeoutId !== this.pollTimeoutId) {
+        return; // ignore if polling was deferred
+      }
 
-      this.platform.log.debug('Triggering motionSensorOneService:', motionDetected);
-      this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
-    }, 10000);
+      if (chargerState.state === 'error' || chargerState.state === 'disconnect') {
+        throw new Error('chargerState.state = ' + chargerState.state);
+      }
+
+      // this.platform.log.debug(chargerState);
+
+      this.state = chargerState;
+      this.error = null;
+      this.overrides = {};
+      // this does not work dynamically:
+      // this.service.getCharacteristic(this.platform.Characteristic.On).setProps({
+      //   perms: chargerState.state === 'standby' ? [Perms.PAIRED_READ, Perms.NOTIFY] : [Perms.PAIRED_WRITE, Perms.PAIRED_READ, Perms.NOTIFY],
+      // });
+      this.service.updateCharacteristic(this.platform.Characteristic.On, await this.getOn());
+      this.service.updateCharacteristic(this.platform.Characteristic.OutletInUse, await this.getOutletInUse());
+      this.service.updateCharacteristic(this.platform.Volts, await this.getVolts());
+      this.service.updateCharacteristic(this.platform.Amperes, await this.getAmperes());
+      this.service.updateCharacteristic(this.platform.Watts, await this.getWatts());
+      this.service.updateCharacteristic(this.platform.KilowattHours, await this.getKilowattHours());
+      // this.service.updateCharacteristic(this.platform.VoltAmperes, await this.getVoltAmperes());
+      // this.service.updateCharacteristic(this.platform.KilowattVoltAmpereHour, await this.getKilowattVoltAmpereHour());
+
+    } catch (e) {
+      this.platform.log.error(e);
+      this.state = {};
+      this.error = e;
+      this.overrides = {};
+    }
+  }
+
+  private async handleJuiceNetErrors(promise) {
+    let ret;
+    try {
+      ret = await promise;
+    } catch (e) {
+      this.platform.log.error(e);
+      throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+    }
+
+    if (!ret.response.body.success) {
+      this.platform.log.error(ret.response.body);
+      throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+    }
+
+    return ret;
   }
 
   /**
@@ -92,17 +140,35 @@ export class JuiceBoxPlatformAccessory {
    * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
    */
   async setOn(value: CharacteristicValue) {
-    // implement your own code to turn your device on/off
-    this.exampleStates.On = value as boolean;
+    // todo: handle setting errors in http and in chargerState.success
+    this.platform.log.debug('Set Characteristic On ->', value, typeof value);
 
-    this.platform.log.debug('Set Characteristic On ->', value);
+    // todo: how to handle feedback for when car not plugged in
+    this.pausePolling(30 * 1000);
+    await this.poll();
+    // can't turn on a charger that isn't plugged in
+    if (this.error || (value && this.state.state === 'standby')) {
+      this.platform.log.debug('...rejecting set');
+      throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+    }
+
+    this.overrides.on = value;
+    if (value) {
+      await this.handleJuiceNetErrors(juicenet.startChargingAsync(this.platform.config.apiToken, this.accessory.context.device.token));
+    } else {
+      await this.handleJuiceNetErrors(juicenet.stopChargingAsync(this.platform.config.apiToken, this.accessory.context.device.token));
+    }
+
+    // set seems to take ~30+=10s to "take" during which it shows the old state (for some state transitions)
+    // this may be related to the device's update interval?
+    this.pausePolling(30 * 1000);
   }
 
   /**
    * Handle the "GET" requests from HomeKit
    * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
    *
-   * GET requests should return as fast as possbile. A long delay here will result in
+   * GET requests should return as fast as possible. A long delay here will result in
    * HomeKit being unresponsive and a bad user experience in general.
    *
    * If your device takes time to respond you should update the status of your device
@@ -112,26 +178,87 @@ export class JuiceBoxPlatformAccessory {
    * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
    */
   async getOn(): Promise<CharacteristicValue> {
-    // implement your own code to check if the device is on
-    const isOn = this.exampleStates.On;
+    if (this.error) {
+      throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+    }
+    if (this.overrides.on) {
+      const isOn = this.overrides.on;
+      this.platform.log.debug('Get Characteristic On (override) ->', isOn);
+      return isOn;
+    }
 
+    // Also show On for a plugged in car that is fully charged
+    // todo: verify this in the TOU timeframe
+    const isOn = this.state.state === 'charging' || (this.state.state === 'plugged' && this.state.target_time === this.state.default_target_time);
     this.platform.log.debug('Get Characteristic On ->', isOn);
 
-    // if you need to return an error to show the device as "Not Responding" in the Home app:
-    // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+    // TODO; handle other states
 
     return isOn;
   }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, changing the Brightness
-   */
-  async setBrightness(value: CharacteristicValue) {
-    // implement your own code to set the brightness
-    this.exampleStates.Brightness = value as number;
+  async getOutletInUse(): Promise<CharacteristicValue> {
+    if (this.error) {
+      throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+    }
 
-    this.platform.log.debug('Set Characteristic Brightness -> ', value);
+    const OutletInUse = this.state.state === 'charging' || this.state.state === 'plugged';
+    this.platform.log.debug('Get Characteristic OutletInUse ->', OutletInUse);
+
+    return OutletInUse;
   }
 
+  async getVolts(): Promise<CharacteristicValue> {
+    if (this.error) {
+      throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+    }
+    const volts = this.state.charging.voltage;
+    this.platform.log.debug('Get Characteristic Volts ->', volts);
+    return volts;
+  }
+
+  async getAmperes(): Promise<CharacteristicValue> {
+    if (this.error) {
+      throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+    }
+    const amps = this.state.charging.amps_current;
+    this.platform.log.debug('Get Characteristic Amperes ->', amps);
+    return amps;
+  }
+
+  async getWatts(): Promise<CharacteristicValue> {
+    if (this.error) {
+      throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+    }
+    const value = this.state.charging.watt_power;
+    this.platform.log.debug('Get Characteristic Watts ->', value);
+    return value;
+  }
+
+  async getKilowattHours(): Promise<CharacteristicValue> {
+    if (this.error) {
+      throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+    }
+    const value = this.state.charging.wh_energy / 1000;
+    this.platform.log.debug('Get Characteristic KilowattHours ->', value);
+    return value;
+  }
+
+  // async getVoltAmperes(): Promise<CharacteristicValue> {
+  //   if (this.error) {
+  //     throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+  //   }
+  //   const value = 0;
+  //   this.platform.log.debug('Get Characteristic VoltAmperes ->', value);
+  //   return value;
+  // }
+
+  // async getKilowattVoltAmpereHour(): Promise<CharacteristicValue> {
+  //   if (this.error) {
+  //     throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+  //   }
+  //   const value = 0;
+  //   this.platform.log.debug('Get Characteristic KilowattVoltAmpereHour ->', value);
+  //   return value;
+  // }
 }
